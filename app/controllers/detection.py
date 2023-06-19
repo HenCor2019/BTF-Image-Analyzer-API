b9bc0e4002b098d8050ffc16bd3451e0b87d90ca
Raul Ernesto Guillen Hernandez <00012119@uca.edu.sv>
2023-06-18 23:04:50 -0600

    :bug: Solved operation order - Cropped then resize

--- app/controllers/detection.py
+++ app/controllers/detection.py
@@ -16,9 +16,11 @@ from app.utils.image_processing import read_image_from_file
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+
 config = cloudinary.config(secure=True)
 
 router = APIRouter()
+
 
 @router.post("/api/v1/{patient_id}/analyze", summary="Use it to get an image predictions", tags=["Diagnostics"])
 async def analyze_image(
@@ -35,10 +37,10 @@ async def analyze_image(
             )
 
         normalized_image = await read_image_from_file(file)
-        instances = normalized_image.reshape((1,)+normalized_image.shape).tolist()
+        instances = normalized_image.reshape((1,) + normalized_image.shape).tolist()
         request_json = json.dumps({
-             "signature_name": "serving_default",
-             "instances": instances
+            "signature_name": "serving_default",
+            "instances": instances
         })
         response = requests.post(DETECTION_MODEL_BASE_URL, data=request_json)
         response.raise_for_status()
@@ -48,7 +50,7 @@ async def analyze_image(
         await file.seek(0)
         result = cloudinary.uploader.upload(file.file)
         src_url = result.get("url")
-        DiagnosticService().create_one(doctor.id,patient_id, src_url, predictions[1], predictions[0])
+        DiagnosticService().create_one(doctor.id, patient_id, src_url, predictions[1], predictions[0])
         return JSONResponse(
             status_code=200,
             content={'tumor_detection': {'no_tumor': predictions[0], 'tumor': predictions[1]}}
@@ -64,11 +66,10 @@ async def analyze_image(
 async def validate_image(user: User = Depends(get_current_user), file: UploadFile = File(default=None)):
     try:
         normalized_image = await read_image_from_file(file)
-
-        instances = normalized_image.reshape((1,)+normalized_image.shape).tolist()
+        instances = normalized_image.reshape((1,) + normalized_image.shape).tolist()
         request_json = json.dumps({
-             "signature_name": "serving_default",
-             "instances": instances
+            "signature_name": "serving_default",
+            "instances": instances
         })
         response = requests.post(VALIDATION_MODEL_BASE_URL, data=request_json)
         response.raise_for_status()
@@ -78,10 +79,10 @@ async def validate_image(user: User = Depends(get_current_user), file: UploadFil
         is_valid_mri_image = 1.0 - float(mri_image_percentage) > float(MRI_VALIDATION_THRESHOLD)
         return JSONResponse(
             status_code=200,
-            content={'is_valid_mri_image': is_valid_mri_image, 'percentage': mri_image_percentage }
+            content={'is_valid_mri_image': is_valid_mri_image, 'percentage': mri_image_percentage}
         )
     except:
         return JSONResponse(
             status_code=400,
-            content={'is_valid_mri_image': False, 'percentage': -1 }
+            content={'is_valid_mri_image': False, 'percentage': -1}
         )
