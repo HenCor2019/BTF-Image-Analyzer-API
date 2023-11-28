4fafb38732cc8bc81d6022b8e371dba0080d8ae9
Henry Cortez <Henry200amaya@gmail.com>
2023-11-28 11:43:31 -0600

    :sparkles: Update docker compose

--- app/constants/tensorflow_url.py
+++ app/constants/tensorflow_url.py
@@ -1,6 +1,6 @@
 from cv2 import os
 
 
-DETECTION_MODEL_BASE_URL = os.getenv("DETECTION_API_BASE_URL", "http://model:8505/v1/models/brain_tumor_validator:predict")
+DETECTION_MODEL_BASE_URL = os.getenv("DETECTION_API_BASE_URL", "http://model:8501/v1/models/brain_tumor_validator:predict")
 VALIDATION_MODEL_BASE_URL = os.getenv("VALIDATION_API_BASE_URL", "http://model_validator:8505/v1/models/brain_tumor_validator:predict")
 MRI_VALIDATION_THRESHOLD = os.getenv("MRI_VALIDATION_THRESHOLD", 0.6)
